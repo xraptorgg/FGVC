50656d8240afa8f585e70891ec456c1b22d89096
xRaptorGG <x.shayan.halder@gmail.com>
2023-04-10 13:35:05 +0530

    Bug fixes

--- data_utils/dataset.py
+++ data_utils/dataset.py
@@ -3,6 +3,7 @@
 import os
 import numpy as np
 import torch
+import pandas as pd
 from PIL import Image
 import scipy.io
 from torch.utils.data import Dataset
@@ -11,7 +12,7 @@ from torch.utils.data import Dataset
 
 class StanfordCars(Dataset):
     def __init__(self, data_dir, mat_annos, data_annos, transform = None):
-        self.car_annotations = scipy.io.loadmat(data_annos)["annotations"][0]
+        self.car_annotations = pd.read_csv(data_annos)
         
         self.class_names = [name[0].replace("/", "-") for name in scipy.io.loadmat(mat_annos)["class_names"][0]]
         
@@ -22,10 +23,9 @@ class StanfordCars(Dataset):
         return len(self.car_annotations)
     
     def __getitem__(self, index):
-        image_name = os.path.join(self.data_dir, self.car_annotations[index][-1][0])
+        image_name = os.path.join(self.data_dir, self.car_annotations['name'][index])
         image = Image.open(image_name).convert('RGB')
-        car_class = self.car_annotations[index][-2][0][0]
-        car_class = torch.from_numpy(np.array(car_class.astype(np.float32))).long() - 1
+        car_class = self.car_annotations['class_id'][index]
         
         if self.transform:
             image = self.transform(image)
